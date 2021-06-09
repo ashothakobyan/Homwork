84f3e2c9a5d3e1ef52d2c1dc1359564ecbc4bffb
ashothakobyan <85190441+ashothakobyan@users.noreply.github.com>
2021-06-09 22:20:15 +0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,72 @@
+1
+
+let number
+if(number%2 === 0){
+    console.log("even")
+}else{
+    console.log("odd")
+    }
+
+
+2 - first versia
+
+let number
+if(number<10 && number>0){
+console.log(number)
+}
+else if (number%10===0 && number>0){
+    console.log(number)
+}else if(number%10!==0 && number<1000 && number>0){
+    console.log(100*(number%10) + ((number-number%10)/10))
+}else if(number%10!==0 && number<10000 && number>0){
+    console.log(1000*(number%10) + ((number-number%10)/10))
+}
+
+2 - second versia 
+
+let number
+if(number<10 && number>0){
+console.log(number)
+}
+else if (number%10===0 && number>0){
+    console.log(number)
+}else if(number%10!==0 && number>0){
+    console.log(number%10 +''+ ((number-number%10)/10))
+}
+
+3
+
+let number, number1,number2,number3,number4
+console.log("the average of this number is equal " + (number+number1+number2+number3+number4)/5 )
+
+4
+
+let number,number1,number2
+if(number > number1 && number > number2 && number1>number2){
+    console.log(number,number1,number2)
+}else if(number > number1 && number > number2 && number2>number1){
+    console.log(number,number2,number1)
+}else if(number1 >number && number1>number2 && number > number2){
+    console.log(number1,number,number2)
+}else if(number1 > number && number1 > number2 && number2 > number){
+    console.log(number1,number2,number)
+}else if(number2 > number && number2 > number1 && number > number1){
+    console.log(number2,number,number1)
+}else {
+    console.log(number2,number1,number)
+}
+
+5
+
+var n = +prompt()
+
+var i = 0
+var j = 0
+
+if (n % 2 === 0 && !Math.floor(n / 10)){
+    i += 1
+}
+
+if (n % 3 === 0 && n % 10 === 1){
+    j += 1
+}
