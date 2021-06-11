19b34909b6652400cd1803131b4fe3c8fdac4f69
ashothakobyan <85190441+ashothakobyan@users.noreply.github.com>
2021-06-11 15:36:45 +0400

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -1,11 +1,10 @@
 1.....
 
-let number
-    if(number%2 === 0){
-        console.log("even")
-    }else{
-        console.log("odd")
-        }
+let number1,number2 
+    number1 = number1 + number2
+    number2 = number1 - number2
+    number1 = number1 - number2
+console.log(number1,number2)
 
 
 2 ...... 
@@ -20,20 +19,20 @@ let number= +prompt(), z=number, y=1
 3 .......
 
 let number, number1,number2,number3,number4
-console.log("the average of this number is equal " + (number+number1+number2+number3+number4)/5 )
+    console.log("the average of this number is equal " + (number+number1+number2+number3+number4)/5 )
 
 4 .....
 
 let number,number1,number2
-if(number > number1 && number > number2 && number1>number2){
-    console.log(number,number1,number2)
-}else if(number > number1 && number > number2 && number2>number1){
-    console.log(number,number2,number1)
-}else if(number1 >number && number1>number2 && number > number2){
-    console.log(number1,number,number2)
-}else if(number1 > number && number1 > number2 && number2 > number){
-    console.log(number1,number2,number)
-}else if(number2 > number && number2 > number1 && number > number1){
+    if(number > number1 && number > number2 && number1>number2){
+        console.log(number,number1,number2)
+    }else if(number > number1 && number > number2 && number2>number1){
+        console.log(number,number2,number1)
+    }else if(number1 >number && number1>number2 && number > number2){
+        console.log(number1,number,number2)
+    }else if(number1 > number && number1 > number2 && number2 > number){
+        console.log(number1,number2,number)
+    }else if(number2 > number && number2 > number1 && number > number1){
     console.log(number2,number,number1)
 }else {
     console.log(number2,number1,number)
