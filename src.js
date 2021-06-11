1514643ee676f75e69acd6ee1dfdbcdbe479e8cb
ashothakobyan <85190441+ashothakobyan@users.noreply.github.com>
2021-06-11 14:55:45 +0400

    Update src.js

--- src.js
+++ src.js
@@ -1,1 +1,9 @@
-zxczxzxzc
+let number= +prompt(), z=number, y=1
+if(number>10 || number%10 !== 0){
+    while (z > 0){
+(y=y*10)  z=(z-z%10)/10 
+}
+    console.log((y/10)*(number%10)+(number-number%10)/10)}
+else if(number<10 && number>0 || number%10 === 0 ){
+    console.log(number)
+}
