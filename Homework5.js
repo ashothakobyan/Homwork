7b07418ddb40d7c69ed8dc972d5fdfe543176874
ashothakobyan <85190441+ashothakobyan@users.noreply.github.com>
2022-04-20 18:52:34 +0400

    Update Homework5.js

--- Homework5.js
+++ Homework5.js
@@ -1,3 +1,100 @@
+class MyPromise {
+    constructor(handler) {
+        this.status = "pending";
+        this.onFulfilledCallbacks = [];
+        this.onRejectedCallbacks = [];
+
+        const resolve = value => {
+            if (this.status === "pending") {
+                this.status = "fulfilled";
+                this.value = value;
+                this.onFulfilledCallbacks.forEach(fn => fn(value));
+            }
+        };
+
+        const reject = value => {
+            if (this.status === "pending") {
+                this.status = "rejected";
+                this.value = value;
+                this.onRejectedCallbacks.forEach(fn => fn(value));
+            }
+        };
+
+        try {
+            handler(resolve, reject);
+        } catch (err) {
+            reject(err);
+        }
+    }
+
+    then(onFulfilled, onRejected) {
+        return new MyPromise((resolve, reject) => {
+            if (this.status === "pending") {
+                this.onFulfilledCallbacks.push(() => {
+                    try {
+                        const fulfilledFromLastPromise = onFulfilled(this.value);
+                        if (fulfilledFromLastPromise instanceof MyPromise) {
+                            fulfilledFromLastPromise.then(resolve, reject);
+                        } else {
+                            resolve(fulfilledFromLastPromise);
+                        }
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
+                this.onRejectedCallbacks.push(() => {
+                    try {
+                        const rejectedFromLastPromise = onRejected(this.value);
+                        if (rejectedFromLastPromise instanceof MyPromise) {
+                            rejectedFromLastPromise.then(resolve, reject);
+                        } else {
+                            reject(rejectedFromLastPromise);
+                        }
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
+            }
+
+            if (this.status === "fulfilled") {
+                try {
+                    const fulfilledFromLastPromise = onFulfilled(this.value);
+                    if (fulfilledFromLastPromise instanceof MyPromise) {
+                        fulfilledFromLastPromise.then(resolve, reject);
+                    } else {
+                        resolve(fulfilledFromLastPromise);
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+
+            }
+
+            if (this.status === "rejected") {
+                try {
+                    const rejectedFromLastPromise = onRejected(this.value);
+                    if (rejectedFromLastPromise instanceof MyPromise) {
+                        rejectedFromLastPromise.then(resolve, reject);
+                    } else {
+                        reject(rejectedFromLastPromise);
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
+
+
 function allPosibleSubsets(arr,length,x = -1){
     if(arr.length <= length){
         return [arr]
